224efeb18d34828b1e3696efb9ebceb21ee8f99d
Axel Haustant <noirbizarre@gmail.com>
2012-08-03 21:37:30 +0200

    Try harder

--- travis_mapnik.sh
+++ travis_mapnik.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 # Dirty ugly hack to use mapnik system installation
 
-cp -R /usr/share/pyshared/mapnik2 $VIRTUAL_ENV/lib/python2.7/site-packages/
-# cp /usr/lib/pyshared/python2.7/mapnik2/* $VIRTUAL_ENV/lib/python2.7/site-packages/mapnik2
+if [ -d "/usr/share/pyshared/mapnik" ]; then
+    echo "Copying /usr/share/pyshared/mapnik into virtualenv"
+    cp -R /usr/share/pyshared/mapnik $VIRTUAL_ENV/lib/python2.7/site-packages/
+fi
+
+if [ -d "/usr/share/pyshared/mapnik2" ]; then
+    echo "Copying /usr/share/pyshared/mapnik2 into virtualenv"
+    cp -R /usr/share/pyshared/mapnik2 $VIRTUAL_ENV/lib/python2.7/site-packages/
+fi
+
+if [ -d "/usr/lib/pyshared/python2.7/mapnik" ]; then
+    echo "/usr/lib/pyshared/python2.7/mapnik/* into virtualenv"
+    if [ -d "$VIRTUAL_ENV/lib/python2.7/site-packages/mapnik" ]; then
+        mkdir -p $VIRTUAL_ENV/lib/python2.7/site-packages/mapnik
+    fi
+    cp /usr/lib/pyshared/python2.7/mapnik/* $VIRTUAL_ENV/lib/python2.7/site-packages/mapnik
+fi
+
+if [ -d "/usr/lib/pyshared/python2.7/mapnik2" ]; then
+    echo "/usr/lib/pyshared/python2.7/mapnik2/* into virtualenv"
+    if [ -d "$VIRTUAL_ENV/lib/python2.7/site-packages/mapnik2" ]; then
+        mkdir -p $VIRTUAL_ENV/lib/python2.7/site-packages/mapnik2
+    fi
+    cp /usr/lib/pyshared/python2.7/mapnik2/* $VIRTUAL_ENV/lib/python2.7/site-packages/mapnik2
+fi
