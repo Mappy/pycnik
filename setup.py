92e298fffb929dea72b4616a24ed3cfcdab42764
Axel Haustant <axel.haustant.ext@mappy.com>
2012-09-11 11:01:46 +0200

    Bump version 1.2

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ def install_requires():
 
 setup(
     name='pycnik',
-    version='1.1',
+    version='1.2',
     description="Tool for generating Mapnik's stylesheets from python code",
     long_description=descr,
     url="https://github.com/ldgeo/pycnik.git",
